2fd7c08efc6f6e061a3eb0dabd41763a9ec37c51
kimngoc280105 <165563517+kimngoc280105@users.noreply.github.com>
2025-09-17 00:57:49 +0700

    Update sd.sh

--- sd/sd.sh
+++ sd/sd.sh
@@ -1,18 +1,7 @@
-#!/bin/bash
-set -e
-
-# Về thư mục gốc
-cd /content
-
-# Kích hoạt môi trường ảo
-source bayesdiff_env/bin/activate
-
-# Vào thư mục Stable Diffusion
-cd BayesDiff/sd
-
-# ============================
-# 1. Cooperate UQ into ddim sampler
-# ============================
+# Cooperate UQ into ddim sampler, experiment results will be saved in ./ddim_exp/skipUQ/
+CUDA_VISIBLE_DEVICES=0 python ddim_skipUQ.py --prompt "your_prompt" \
+--ckpt your_local_model_path --local_image_path your_local_image_path --laion_art_path your_laion_art_path \
+--H 512 --W 512 --scale 3 --train_la_data_size 1000 --train_la_batch_size 10 \
 CUDA_VISIBLE_DEVICES=0 python ddim_skipUQ.py \
 --prompt "A futuristic city with flying cars, ultra realistic" \
 --ckpt /content/BayesDiff/sd/your_local_model_path/sd-v1-5.ckpt \
@@ -22,9 +11,10 @@ CUDA_VISIBLE_DEVICES=0 python ddim_skipUQ.py \
 --train_la_data_size 1000 --train_la_batch_size 10 \
 --sample_batch_size 2 --total_n_samples 48 --timesteps 50
 
-# ============================
-# 2. Cooperate UQ into dpm-solver-2 sampler
-# ============================
+# Cooperate UQ into dpm-solver-2 sampler, experiment results will be saved in ./dpm_solver_2_exp/skipUQ/
+CUDA_VISIBLE_DEVICES=1 python dpmsolver_skipUQ.py --prompt "your_prompt" \
+--ckpt your_local_model_path --local_image_path your_local_image_path --laion_art_path your_laion_art_path \
+--H 512 --W 512 --scale 3 --train_la_data_size 1000 --train_la_batch_size 10 \
 CUDA_VISIBLE_DEVICES=0 python dpmsolver_skipUQ.py \
 --prompt "A futuristic city with flying cars, ultra realistic" \
 --ckpt /content/BayesDiff/sd/your_local_model_path/sd-v1-5.ckpt \
