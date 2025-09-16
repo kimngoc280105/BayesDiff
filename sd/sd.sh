d190860625f40d0402f54a3a755fa59394980c5e
kimngoc280105 <165563517+kimngoc280105@users.noreply.github.com>
2025-09-17 00:51:38 +0700

    Update sd.sh

--- sd/sd.sh
+++ sd/sd.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
+set -e
 
-# Cooperate UQ into ddim sampler
-../bayesdiff_env/bin/python ddim_skipUQ.py \
+# Về thư mục gốc
+cd /content
+
+# Kích hoạt môi trường ảo
+source bayesdiff_env/bin/activate
+
+# Vào thư mục Stable Diffusion
+cd BayesDiff/sd
+
+# ============================
+# 1. Cooperate UQ into ddim sampler
+# ============================
+CUDA_VISIBLE_DEVICES=0 python ddim_skipUQ.py \
 --prompt "A futuristic city with flying cars, ultra realistic" \
 --ckpt /content/BayesDiff/sd/your_local_model_path/sd-v1-5.ckpt \
 --local_image_path /content/BayesDiff/sd/your_local_image_path \
@@ -10,8 +22,10 @@
 --train_la_data_size 1000 --train_la_batch_size 10 \
 --sample_batch_size 2 --total_n_samples 48 --timesteps 50
 
-# Cooperate UQ into dpm-solver-2 sampler
-../bayesdiff_env/bin/python dpmsolver_skipUQ.py \
+# ============================
+# 2. Cooperate UQ into dpm-solver-2 sampler
+# ============================
+CUDA_VISIBLE_DEVICES=0 python dpmsolver_skipUQ.py \
 --prompt "A futuristic city with flying cars, ultra realistic" \
 --ckpt /content/BayesDiff/sd/your_local_model_path/sd-v1-5.ckpt \
 --local_image_path /content/BayesDiff/sd/your_local_image_path \
