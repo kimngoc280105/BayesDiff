9f672d2345e4bc8b58d1cd8fc0f219148fb82704
kimngoc280105 <165563517+kimngoc280105@users.noreply.github.com>
2025-09-16 19:39:02 +0700

    Update sd.sh

--- sd/sd.sh
+++ sd/sd.sh
@@ -1,11 +1,19 @@
 # Cooperate UQ into ddim sampler, experiment results will be saved in ./ddim_exp/skipUQ/
-CUDA_VISIBLE_DEVICES=0 python ddim_skipUQ.py --prompt "your_prompt" \
---ckpt your_local_model_path --local_image_path your_local_image_path --laion_art_path your_laion_art_path \
---H 512 --W 512 --scale 3 --train_la_data_size 1000 --train_la_batch_size 10 \
+CUDA_VISIBLE_DEVICES=0 python ddim_skipUQ.py \
+--prompt "A futuristic city with flying cars, ultra realistic" \
+--ckpt /content/BayesDiff/sd/your_local_model_path/sd-v1-5.ckpt \
+--local_image_path /content/BayesDiff/sd/your_local_image_path \
+--laion_art_path /content/BayesDiff/sd/your_laion_art_path \
+--H 512 --W 512 --scale 3 \
+--train_la_data_size 1000 --train_la_batch_size 10 \
 --sample_batch_size 2 --total_n_samples 48 --timesteps 50
 
 # Cooperate UQ into dpm-solver-2 sampler, experiment results will be saved in ./dpm_solver_2_exp/skipUQ/
-CUDA_VISIBLE_DEVICES=1 python dpmsolver_skipUQ.py --prompt "your_prompt" \
---ckpt your_local_model_path --local_image_path your_local_image_path --laion_art_path your_laion_art_path \
---H 512 --W 512 --scale 3 --train_la_data_size 1000 --train_la_batch_size 10 \
+CUDA_VISIBLE_DEVICES=0 python dpmsolver_skipUQ.py \
+--prompt "A futuristic city with flying cars, ultra realistic" \
+--ckpt /content/BayesDiff/sd/your_local_model_path/sd-v1-5.ckpt \
+--local_image_path /content/BayesDiff/sd/your_local_image_path \
+--laion_art_path /content/BayesDiff/sd/your_laion_art_path \
+--H 512 --W 512 --scale 3 \
+--train_la_data_size 1000 --train_la_batch_size 10 \
 --sample_batch_size 2 --total_n_samples 48 --timesteps 50
