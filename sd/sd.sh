e847ced643e4c86e0f30ec1cfe6a2923c2ee9b04
kimngoc280105 <165563517+kimngoc280105@users.noreply.github.com>
2025-09-25 15:59:14 +0700

    Update sd.sh

--- sd/sd.sh
+++ sd/sd.sh
@@ -1,33 +1,28 @@
 #!/bin/bash
 PYTHON=python3
 
-# ============================
-# 1. Tải model từ Hugging Face
-# ============================
 mkdir -p models
 wget -O models/v1-5-pruned.ckpt https://huggingface.co/kngoc281/sd-v1-5-pruned/resolve/main/v1-5-pruned.ckpt
 wget -O models/v1-inference.yaml https://huggingface.co/kngoc281/sd-v1-5-pruned/resolve/main/v1-inference.yaml
 
-# ============================
-# 2. Chạy với ddim_skipUQ.py
-# ============================
 CUDA_VISIBLE_DEVICES=0 $PYTHON ddim_skipUQ.py \
---prompt "A futuristic city with flying cars, ultra realistic" \
---ckpt models/v1-5-pruned.ckpt \
---local_image_path /content/BayesDiff/sd/your_local_image_path \
---laion_art_path /content/BayesDiff/sd/your_laion_art_path \
---H 512 --W 512 --scale 3 \
---train_la_data_size 1000 --train_la_batch_size 10 \
---sample_batch_size 2 --total_n_samples 48 --timesteps 50
+  --prompt "A futuristic city with flying cars, ultra realistic" \
+  --ckpt models/v1-5-pruned.ckpt \
+  --config models/v1-inference.yaml \
+  --local_image_path /content/BayesDiff/laion_subset \
+  --laion_art_path /content/drive/MyDrive/sd/laion-art.txt \
+  --H 256 --W 256 --scale 3 \
+  --train_la_data_size 50 --train_la_batch_size 2 \
+  --sample_batch_size 1 --total_n_samples 4 --timesteps 20 \
+  --precision autocast
 
-# ============================
-# 3. Chạy với dpmsolver_skipUQ.py
-# ============================
 CUDA_VISIBLE_DEVICES=0 $PYTHON dpmsolver_skipUQ.py \
---prompt "A futuristic city with flying cars, ultra realistic" \
---ckpt models/v1-5-pruned.ckpt \
---local_image_path /content/BayesDiff/sd/your_local_image_path \
---laion_art_path /content/BayesDiff/sd/your_laion_art_path \
---H 512 --W 512 --scale 3 \
---train_la_data_size 1000 --train_la_batch_size 10 \
---sample_batch_size 2 --total_n_samples 48 --timesteps 50
+  --prompt "A futuristic city with flying cars, ultra realistic" \
+  --ckpt models/v1-5-pruned.ckpt \
+  --config models/v1-inference.yaml \
+  --local_image_path /content/BayesDiff/laion_subset \
+  --laion_art_path /content/drive/MyDrive/sd/laion-art.txt \
+  --H 256 --W 256 --scale 3 \
+  --train_la_data_size 50 --train_la_batch_size 2 \
+  --sample_batch_size 1 --total_n_samples 4 --timesteps 20 \
+  --precision autocast
